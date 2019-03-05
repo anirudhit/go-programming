291e18111a4586fcfa7e0ebf11bd18b86fa1151f
anirudh bellamkonda <anirudhit@outlook.com>
2019-03-05 01:34:21 -0600

    pl-assignment-1
    - Added sort functionality
    - Mapped the overall batting average
    - Updated the results display

--- src/pl-assignment-1/playerStatistics.go
+++ src/pl-assignment-1/playerStatistics.go
@@ -3,23 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"math"
 	"os"
+	"sort"
 	"strconv"
 )
-
-type Player struct {
-	FirstName        string
-	LastName         string
-	PlateAppearances int
-	AtBats           int
-	Singles          int
-	Doubles          int
-	Triples          int
-	HomeRuns         int
-	Walks            int
-	HitByPitch       int
-}
 
 type PlayerStats struct {
 	FirstName string
@@ -29,15 +16,26 @@ type PlayerStats struct {
 	OnBase    float64
 }
 
+type ByLastName []PlayerStats
+
+func (a ByLastName) Len() int           { return len(a) }
+func (a ByLastName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByLastName) Less(i, j int) bool { return a[i].LastName < a[j].LastName }
+
 func main() {
 
-	var welcomeMessage = "Welcome to the player statisticscalculator test program.  I am going to \nread playersfrom an input data file." +
-		"You will tell me the names of \nyour input and output files.  I will store all of the playersin a list,\ncompute each player's averages" +
+	var welcomeMessage = "Welcome to the player statistics calculator test program. " +
+		"I am going to \nread playersfrom an input data file." +
+		"You will tell me the names of \nyour input and output files. " +
+		"I will store all of the playersin a list,\ncompute each player's averages " +
 		"and then write the resulting team report to \nyour output file.\n\n"
 	fmt.Println(welcomeMessage)
 
 	// Taking the file input name
 	var FileName string
+	var TotalPlayers int
+	var BattingAverage float64
+	var OverallBattingAverage float64
 	fmt.Print("Enter the name of your input file:	")
 	fmt.Scan(&FileName)
 
@@ -54,39 +52,14 @@ func main() {
 	reader.FieldsPerRecord = -1
 
 	playerData, err := reader.ReadAll()
+	TotalPlayers = len(playerData)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// fmt.Println("playerData: ", len(playerData))
-
-	// var onePlayer Player
-	// var allPlayers []Player
-
-	// for _, eachPlayer := range playerData {
-	// 	onePlayer.FirstName = eachPlayer[0]
-	// 	onePlayer.LastName = eachPlayer[1]
-	// 	onePlayer.PlateAppearances, _ = strconv.Atoi(eachPlayer[2])
-	// 	onePlayer.AtBats, _ = strconv.Atoi(eachPlayer[3])
-	// 	onePlayer.Singles, _ = strconv.Atoi(eachPlayer[4])
-	// 	onePlayer.Doubles, _ = strconv.Atoi(eachPlayer[5])
-	// 	onePlayer.Triples, _ = strconv.Atoi(eachPlayer[6])
-	// 	onePlayer.HomeRuns, _ = strconv.Atoi(eachPlayer[7])
-	// 	onePlayer.Walks, _ = strconv.Atoi(eachPlayer[8])
-	// 	onePlayer.HitByPitch, _ = strconv.Atoi(eachPlayer[9])
-
-	// 	allPlayers = append(allPlayers, onePlayer)
-	// }
-
 	var onePlayerStats PlayerStats
 	var allPlayerStats []PlayerStats
-
-	fmt.Println("BASEBALL TEAM REPORT --- ", len(playerData), " PLAYERS FOUND IN FILE")
-	fmt.Println("OVERALL BATTING AVERAGE is 0.290")
-
-	fmt.Println("\n \t PLAYER NAME \t\t:\t AVERAGE\tSLUGGING\tONBASE%")
-	fmt.Println("-------------------------------------------------------------------------------")
 
 	for _, eachPlayer := range playerData {
 		onePlayerStats.FirstName = eachPlayer[0]
@@ -105,28 +78,24 @@ func main() {
 		onePlayerStats.Slugging = ((Singles) + (2 * Doubles) + (3 * Triples) + (4 * HomeRuns)) / (AtBats)
 		onePlayerStats.OnBase = (Singles + Doubles + Triples + HomeRuns + Walks + HitByPitch) / (PlateAppearances)
 
+		BattingAverage = BattingAverage + onePlayerStats.Average
 		allPlayerStats = append(allPlayerStats, onePlayerStats)
-
-		fmt.Print("\t", onePlayerStats.LastName, ", ")
-		fmt.Print(onePlayerStats.FirstName, "\t\t:")
-		fmt.Print("\t", toFixed(onePlayerStats.Average, 3), "\t")
-		fmt.Print("\t", toFixed(onePlayerStats.Slugging, 3), "\t")
-		fmt.Print("\t", toFixed(onePlayerStats.OnBase, 3), "\n")
 	}
+	OverallBattingAverage = BattingAverage / float64(TotalPlayers)
+	sort.Sort(ByLastName(allPlayerStats))
 
-	// jsondata, err := json.Marshal(allPlayers) // convert to JSON
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	// fmt.Println("1.", string(jsondata))
-}
+	fmt.Println("BASEBALL TEAM REPORT --- ", TotalPlayers, " PLAYERS FOUND IN FILE")
+	fmt.Print("OVERALL BATTING AVERAGE is ")
+	fmt.Printf("%.3f\n", OverallBattingAverage)
 
-func round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
+	fmt.Println("\n \t PLAYER NAME \t\t:\t AVERAGE\tSLUGGING\tONBASE%")
+	fmt.Println("-------------------------------------------------------------------------------")
 
-func toFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
+	for _, eachPlayer := range allPlayerStats {
+		fmt.Print("\t", eachPlayer.LastName, ", ")
+		fmt.Print(eachPlayer.FirstName, "\t\t:")
+		fmt.Printf("\t %.3f \t", eachPlayer.Average)
+		fmt.Printf("\t %.3f \t", eachPlayer.Slugging)
+		fmt.Printf("\t %.3f \n", eachPlayer.OnBase)
+	}
 }
